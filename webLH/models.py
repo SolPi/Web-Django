9bf3122acc80b54c5459a492ef029507786d5084
Adrian Pizarro <adrianpizarroserrano@hotmail.com>
2015-08-10 22:24:32 +0200

    Activity Migrate
    Activity Migrate

--- webLH/models.py
+++ webLH/models.py
@@ -87,4 +87,33 @@ class InfoPermiso(models.Model):
     desc = models.CharField(max_length=2000)
 
     def __str__(self):
-        return self.permiso.permiso.encode('utf-8')
+        return self.permiso.encode('utf-8')
+
+class Activity(models.Model):
+    permiso = models.ForeignKey(Permiso)
+    title = models.CharField(max_length=100, null=True, blank=True)
+    desc = models.CharField(max_length=2000, null=True, blank=True)
+    date = models.DateTimeField('date published', null=True, blank=True)
+    priority = models.IntegerField(default=2) #1: PRIORITY_ACTIVITY_HIGHLIGHT, 2: PRIORITY_ACTIVITY_NORMAL, 3: PRIORITY_ACTIVITY_OLD
+    urlInPage = models.CharField(max_length=300, null=True, blank=True)
+
+
+    def __str__(self):
+        return " titulo actividad:" +self.title.encode('utf-8') + " fecha:" + self.date.encode('utf-8')
+
+class Photo(models.Model):
+    actividad = models.ForeignKey(Activity, null=True, blank=True)
+    user = models.ForeignKey(Hermano, null=True, blank=True)
+    urlBigPhoto = models.CharField(max_length=300, null=True, blank=True) # url foto peq
+    urlSmallPhoto = models.CharField(max_length=300, null=True, blank=True) # rul foto grande
+    isPagPpal = models.IntegerField(default=0) # 1: true 0: false
+    isForActivity = models.IntegerField(default=0)
+    isForHermano  = models.IntegerField(default=0)
+    isForTitle = models.IntegerField(default=0)
+    isForDescription = models.IntegerField(default=0)
+    isforColumn = models.IntegerField(default=0)
+    priority = models.IntegerField(default=1) #(Prioridad 1 max). Si coincide, la más reciente(por fecha)
+    date = models.DateTimeField('date published', null=True, blank=True)
+
+    def __str__(self):
+        return " Pagina ppal:" + self.isPagPpal + " Pagina activity:" + self.isForActivity + " Es para Hermano:" + self.isForHermano + " URL photo :" +self.urlBigPhoto
