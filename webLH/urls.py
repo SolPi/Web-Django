e3de61c669fad523f85b6ff7ad84d2a30cc64b13
Daniel <garoz.daniel@gmail.com>
2015-09-26 11:08:36 +0200

    peticion de paginas por la query

--- webLH/urls.py
+++ webLH/urls.py
@@ -7,6 +7,11 @@ from webLH import views
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
                        url(r'^login$', views.login, name='login'),
+                       url(r'^inicio$', views.inicio, name='inicio'),
+                       url(r'^actividades$', views.actividades, name='actividades'),
+                       url(r'^socios$', views.socios, name='socios'),
+                       url(r'^contacto$', views.contacto, name='contacto'),
+                       url(r'^nuevo_socio$', views.nuevo_socio, name='nuevo_socio'),
                        url(r'^resgistrar$', views.registrar, name='resgistrar'),
                        url(r'^(?P<question_id>\d+)/confirmar$', views.confirmar, name='confirmar'),
                        )
--- webLH/views.py
+++ webLH/views.py
@@ -10,10 +10,36 @@ from webLH.models import *
 
 # Create your views here.
 def index(request):
-    activity_list = Actividade.objects.order_by('-date')[:]
-   # context = {'activity_list':activity_list}
-    return render(request, 'index.html', {'page': 'actividades', 'activity_list':activity_list})
+    return buildPage(request, {'page': 'inicio'})
+def _index(request):
+    return {}
 
+def inicio(request):
+    return buildPage(request, {'page': 'inicio'})
+def _inicio(request):
+    return {}
+
+def actividades(request):
+    return buildPage(request, {'page': 'actividades'})
+def _actividades(request):
+    activity_list = Actividade.objects.order_by('-date')[:]
+    context = {'activity_list':activity_list}
+    return context
+
+def socios(request):
+    return buildPage(request, {'page': 'socios'})
+def _socios(request):
+    return {}
+
+def contacto(request):
+    return buildPage(request, {'page': 'contacto'})
+def _contacto(request):
+    return {}
+
+def nuevo_socio(request):
+    return buildPage(request, {'page': 'nuevo_socio'})
+def _nuevo_socio(request):
+    return {}
 
 def login(request):
     mail = request.POST['mail']
@@ -24,7 +50,6 @@ def login(request):
     else:
         result = user.isPassCorrect(request.POST['pass'])
         return HttpResponse(json.dumps({'msg': result}), content_type="application/json")
-
 
 def registrar(request):
     mail = request.POST['mail']
@@ -51,7 +76,6 @@ def confirmar(request, userId):
     user.state = STATE_REGISTER_CONFIRM
     return render(request, 'index.html', {'page': 'new_socio'})
 
-def activities(request):
-    activity_list = Actividade.objects.order_by('-date')[:]
-    context = {'activity_list':activity_list}
-    return render(request, 'actividades.html', context)
+def buildPage(request, context):
+    context.append(_index).append(_inicio).append(_actividades).append(_socios).append(_contacto).append(_nuevo_socio)
+    return render(request, 'index.html', context)
